06a899be6204a14c5a7a792e33957b5e693268ef
sinan <s0576403@htw-berlin.de>
2022-10-22 18:28:50 +0200

    1.Roter Test Bugfix

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -104,8 +104,15 @@ public class Calculator {
      * entfernt und der Inhalt fortan als positiv interpretiert.
      */
     public void pressNegativeKey() {
-        screen = screen.startsWith("-") ? screen.substring(1) : "-" + screen;
+        if (screen.startsWith("-")){    //Bugfix für Roten test  screen = screen.startsWith("-") ? screen.substring(1) : "-" + screen;
+            screen.substring(1);
+        } else if (latestOperation.equals("-") || latestOperation.equals("+")){
+            screen = "-" + "0";
+        } else {
+            screen = "-" + screen;
+        }
     }
+
 
     /**
      * Empfängt den Befehl der gedrückten "="-Taste.
